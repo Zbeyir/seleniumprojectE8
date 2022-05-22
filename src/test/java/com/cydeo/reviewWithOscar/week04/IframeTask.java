1c306a3c7326bf9f4f4499a90e9e199dcd48cd77
zübeyir <zeynelzatari@gmail.com>
2022-05-22 20:21:45 +0200

    week04_IframeTask

---
+++ src/test/java/com/cydeo/reviewWithOscar/week04/IframeTask.java
@@ -0,0 +1,36 @@
+package com.cydeo.reviewWithOscar.week04;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class IframeTask {
+
+
+        //1. Go to https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
+        //2. Switch to iframe.
+        //3. Get the text “Double-click me to change my text color.” and verify the text
+
+
+    /*
+    What is Iframe why do we need to handle?
+    Iframe are HTML inside another HTML
+    When you automate, in order to intricate with WebElements
+    inside the iframe we need to get into that frame
+     */
+
+    @Test
+    public void iframeTest(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
+
+
+
+    }
+
+}
