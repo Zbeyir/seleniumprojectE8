3b77b39b6868ec486bb1cf20b36a15991c1fb81e
zübeyir <zeynelzatari@gmail.com>
2022-06-08 23:38:41 +0200

    day12_pom_design_explicit_wait

---
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -0,0 +1,55 @@
+package com.cydeo.tests.day12_pom_design_explicit_wait;
+
+import com.cydeo.pages.DynamicLoad7Page;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WebDriveWaitPractices {
+
+
+    @Test
+    public void dynamic_load_7_test() {
+
+
+        //TC#4 : Dynamically Loaded Page Elements 7
+        //1. Go to https://practice.cydeo.com/dynamic_loading/7
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
+
+
+        //2. Wait until title is “Dynamic title”
+
+        //creating the wait object to be able to create certain condition to wait
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //use the "wait" object to create our expected condition
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
+        /*
+        bu wait method site acilirken load yaziyor dynamic yani sonra gidiyor
+        onun icin kullaniyoruz bunu
+         */
+
+        //3. Assert: Message “Done” is displayed.
+
+        //Create object of DynamicLoad7Page to reach and assert web elements
+        DynamicLoad7Page dynamicLoad7Page = new DynamicLoad7Page();
+
+        Assert.assertTrue(dynamicLoad7Page.doneMessage.isDisplayed());
+
+        //4. Assert: Image is displayed.
+        //Note: Follow POM
+
+        BrowserUtils.sleep(3);  // bunu yazmayinca gecmiyor test
+
+        Assert.assertTrue(dynamicLoad7Page.spongeBobImage.isDisplayed());
+
+        BrowserUtils.sleep(3);
+
+        Driver.closeDriver();
+
+
+    }
+}
